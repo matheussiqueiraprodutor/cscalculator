eace71092a22ba095d40a1fa79805f974d8f5266
matheussiqueiraprodutor <matheussiqueiraprodutor@gmail.com>
2026-09-18 11:32:34 -0300

    Update db-config.js

--- db-config.js
+++ db-config.js
@@ -1,4 +1,4 @@
 window.MM_DB_CONFIG = {
-  url: url: 'https://ofovznrdsnobzyeioksx.supabase.co',
+  url: 'https://ofovznrdsnobzyeioksx.supabase.co',
   publishableKey: 'sb_publishable_0H7QwfVmRqc9jSBetKpr9A__bhqXPvr'
 };
